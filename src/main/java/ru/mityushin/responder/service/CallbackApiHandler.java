97e1442fbcc43d3428e79bf813aed8fdbc5b37f2
l524l <65085630+l524l@users.noreply.github.com>
2020-12-15 22:01:31 +0300

    VK SDK

--- src/main/java/ru/mityushin/responder/service/CallbackApiHandler.java
+++ src/main/java/ru/mityushin/responder/service/CallbackApiHandler.java
@@ -1,6 +1,8 @@
 package ru.mityushin.responder.service;
 
 import com.vk.api.sdk.callback.CallbackApi;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.objects.messages.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,13 @@ public class CallbackApiHandler extends CallbackApi {
 
     @Override
     public void messageNew(Integer groupId, Message message) {
+        try {
+            vkMessageSenderService.send(message);
+        } catch (ClientException e) {
+            e.printStackTrace();
+        } catch (ApiException e) {
+            e.printStackTrace();
+        }
         System.out.println(message.getBody());
     }
 }
