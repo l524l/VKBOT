9a1fcf7e400596cbe0a3aa21e57eea0b5c1e0d9f
l524l <65085630+l524l@users.noreply.github.com>
2020-12-18 11:14:33 +0300

    добавлена отправка фото

--- src/main/java/ru/mityushin/responder/service/VkMessageSenderService.java
+++ src/main/java/ru/mityushin/responder/service/VkMessageSenderService.java
@@ -1,17 +1,29 @@
 package ru.mityushin.responder.service;
 
+import com.vk.api.sdk.actions.Upload;
 import com.vk.api.sdk.client.TransportClient;
 import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.UserActor;
 import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
 import com.vk.api.sdk.objects.messages.Message;
+import com.vk.api.sdk.objects.photos.Photo;
+import com.vk.api.sdk.objects.photos.PhotoUpload;
+import com.vk.api.sdk.objects.photos.responses.GetMessagesUploadServerResponse;
+import com.vk.api.sdk.objects.photos.responses.MessageUploadResponse;
+import com.vk.api.sdk.objects.photos.responses.SaveMessagesPhotoResponse;
 import com.vk.api.sdk.queries.photos.PhotosGetMessagesUploadServerQuery;
+import com.vk.api.sdk.queries.photos.PhotosSaveMessagesPhotoQuery;
 import com.vk.api.sdk.queries.upload.UploadPhotoMessageQuery;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
 import ru.mityushin.responder.config.VKGroupActor;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -33,13 +45,19 @@ public class VkMessageSenderService {
         TransportClient transportClient = HttpTransportClient.getInstance();
         vk = vk = new VkApiClient(transportClient);
         message.setRandomId(new Random().nextInt());
+        GetMessagesUploadServerResponse response = vk.photos().getMessagesUploadServer(vkGroupActor.getGroupActor()).execute();
+        ApplicationContext context = new ClassPathXmlApplicationContext();
+        File file = null;
+        try {
+            file = context.getResource("classpath:paterns/promo.png").getFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        MessageUploadResponse mphoto = vk.upload().photoMessage(response.getUploadUrl().toString(), file).execute();
+        List<SaveMessagesPhotoResponse> photoQuery = vk.photos().saveMessagesPhoto(vkGroupActor.getGroupActor(), mphoto.getPhoto()).hash(mphoto.getHash()).server(mphoto.getServer()).execute();
         vk.setVersion("5.126");
-        /*PhotosGetMessagesUploadServerQuery messagesUploadServer = vk.photos().getMessagesUploadServer(vkGroupActor.getGroupActor());
-        File file = new File("classpath://paterns/demo.png");
-        vk.upload();
-        UploadPhotoMessageQuery uploadPhotoMessageQuery = vk.upload().photoMessage(messagesUploadServer.getUrl(), file);
-        vk.photos().saveMessagesPhoto(vkGroupActor.getGroupActor(),uploadPhotoMessageQuery.executeAsString());
-        System.out.println();*/
-        vk.messages().send(vkGroupActor.getGroupActor()).message(message.getText()).randomId(new Random().nextInt()).peerId(message.getPeerId()).execute();
+
+        String attach = "photo" + photoQuery.get(0).getOwnerId() +"_"+ photoQuery.get(0).getId();
+        vk.messages().send(vkGroupActor.getGroupActor()).message(message.getText()).randomId(new Random().nextInt()).peerId(message.getPeerId()).attachment(attach).execute();
     }
 }
